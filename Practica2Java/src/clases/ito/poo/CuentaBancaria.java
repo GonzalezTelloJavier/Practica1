6cb9548a5e874fa373e172ac65fe00e3eb0f4099
JavierTello <JavierTello@192.168.5.105>
2021-03-05 13:13:24 -0600

    El ejercicio que falta

--- Practica2Java/src/clases/ito/poo/CuentaBancaria.java
+++ Practica2Java/src/clases/ito/poo/CuentaBancaria.java
@@ -3,7 +3,7 @@ package clases.ito.poo;
 import java.time.LocalDate;
 
 public class CuentaBancaria {
-	
+
 	public CuentaBancaria(long numCuenta, String nomCliente, float saldo, LocalDate fechaApertura) {
 		super();
 		this.numCuenta = numCuenta;
@@ -12,90 +12,75 @@ public class CuentaBancaria {
 		this.fechaApertura = fechaApertura;
 	}
 
-
 	private long numCuenta = 0L;
 
 	private String nomCliente = "";
 
 	private float saldo = 0F;
 
-	
 	private LocalDate fechaApertura = null;
 
 	private LocalDate fechaActualizacion = null;
 
 	public CuentaBancaria() {
-	
+
 		super();
-		
+
 	}
 
-	
 	public boolean deposito(float cantidad) {
-		
+
 		boolean deposito = false;
 		return deposito;
-		
+
 	}
 
-	
 	public boolean retiro(float cantidad) {
-		
+
 		boolean retiro = false;
 		return retiro;
-		
+
 	}
 
-	
 	public long getNumCuenta() {
 		return this.numCuenta;
 	}
 
-	
 	public void setNumCuenta(long newNumCuenta) {
 		this.numCuenta = newNumCuenta;
 	}
 
-	
 	public String getNomCliente() {
 		return this.nomCliente;
 	}
 
-	
 	public void setNomCliente(String newNomCliente) {
 		this.nomCliente = newNomCliente;
 	}
 
-	
 	public float getSaldo() {
 		return this.saldo;
 	}
 
-	
 	public void setSaldo(float newSaldo) {
 		this.saldo = newSaldo;
 	}
 
-	
 	public LocalDate getFechaApertura() {
 		return this.fechaApertura;
 	}
 
-	
 	public void setFechaApertura(LocalDate newFechaApertura) {
 		this.fechaApertura = newFechaApertura;
 	}
 
-	
 	public LocalDate getFechaActualizacion() {
 		return this.fechaActualizacion;
 	}
 
-	
 	public void setFechaActualizacion(LocalDate newFechaActualizacion) {
 		this.fechaActualizacion = newFechaActualizacion;
 	}
-
 
 	@Override
 	public String toString() {
--- Practica2Java/src/clases/ito/pooo/MyApp.java
+++ Practica2Java/src/clases/ito/pooo/MyApp.java
@@ -6,10 +6,10 @@ import clases.ito.poo.CuentaBancaria;
 public class MyApp {
 
 	static void run() {
-		System.out.println(new CuentaBancaria(89393L,"MARIO BAUTISTA",15000,LocalDate.of(2021,2,26)));
-		
+		System.out.println(new CuentaBancaria(89393L, "MARIO BAUTISTA", 15000, LocalDate.of(2021, 2, 26)));
 
 	}
+
 	public static void main(String[] args) {
 		run();
 	}
